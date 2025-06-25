28b9b3a89b0d226fe2ce81b49d47e3edd61fa551
Ultr-X <schneida_15@web.de>
2025-06-25 18:19:34 +0200

    Mock Button "Connect yourself" on Bluetooth Page

--- app/src/main/java/com/example/mbsnw_abgabe/BluetoothPage.kt
+++ app/src/main/java/com/example/mbsnw_abgabe/BluetoothPage.kt
@@ -1,10 +1,61 @@
 package com.example.mbsnw_abgabe
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mbsnw_abgabe.ui.theme.MBSNWAbgabeTheme
 
 
 @Composable
 fun BluetoothPage() {
-    Text("meow")
+
+    Scaffold() { innerPadding ->
+        Column (
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = 24.dp, vertical = 16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier.fillMaxWidth()
+            ) { ConnectButton(text = "Connect yourself" , onClick = { /* TODO: Implement Bluetooth connection logic */ } ) }
+        }
+    }
+}
+
+@Composable
+fun ConnectButton(text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(52.dp),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Text(text = text, fontSize = 16.sp)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BluetoothPreview() {
+    MBSNWAbgabeTheme {
+        BluetoothPage()
+    }
 }
